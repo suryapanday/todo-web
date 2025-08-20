d84be1d933d19b01570d6df606c02d2b013380e2
Surya Panday <50222070+suryapanday@users.noreply.github.com>
2025-08-20 12:24:07 +1000

    Update TodoList.tsx

--- src/app/components/todo-list/TodoList.tsx
+++ src/app/components/todo-list/TodoList.tsx
@@ -1,12 +1,26 @@
+
+
 import TodoCard from "../todo-card/TodoCard";
-export default async function TodoList({ todos }: unknown) {
+interface Todo {
+  id: string; // or number
+  title: string;
+  completed?: boolean;
+}
+
+import TodoCard from "../todo-card/TodoCard";
+
+interface TodoListProps {
+  todos: Todo[];
+}
+
+export default async function TodoList({ todos }: TodoListProps) {
   return (
     <div>
-      {/* map todos */}
-
-      {todos?.map((todo: unknown) => (
+      {todos?.map((todo) => (
         <TodoCard key={todo.id} todo={todo} />
       ))}
     </div>
   );
 }
+
+
