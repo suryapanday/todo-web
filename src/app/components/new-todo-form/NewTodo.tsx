67143703c0b98461fcbf5d5310c0ef8c5b65e1cf
Surya Panday <50222070+suryapanday@users.noreply.github.com>
2025-08-20 12:21:55 +1000

    Update NewTodo.tsx

--- src/app/components/new-todo-form/NewTodo.tsx
+++ src/app/components/new-todo-form/NewTodo.tsx
@@ -1,9 +1,21 @@
-export default function NewTodo({ action }: { action: unknown }) {
+export default function NewTodo({
+  action,
+}: {
+  action: (formData: FormData) => void | Promise<void>;
+}) {
   return (
-    // on click
-    <form action={action} className="w-full flex flex-row gap-2  ">
-      <input className="w-full border-[0.25px] p-2 " type="text" name="title" id="title" placeholder="add todo" />
-      <button type="submit" className="border-[0.5px] border-amber-300 px-5 hover:bg-amber-300 cursor-pointer ">
+    <form action={action} className="w-full flex flex-row gap-2">
+      <input
+        className="w-full border-[0.25px] p-2"
+        type="text"
+        name="title"
+        id="title"
+        placeholder="add todo"
+      />
+      <button
+        type="submit"
+        className="border-[0.5px] border-amber-300 px-5 hover:bg-amber-300 cursor-pointer"
+      >
         Add
       </button>
     </form>
